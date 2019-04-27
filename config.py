071bc5a20608fbf0c25a16beb1d59889b4be8ef5
MBkkt <valera.mironow@gmail.com>
2019-04-27 12:46:23 +0300

    add migration and change name db

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = (
             os.environ.get('DATABASE_URL') or
-            'sqlite:///' + os.path.join(basedir, 'app.db')
+            'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
